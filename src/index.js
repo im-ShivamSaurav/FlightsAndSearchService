723e768ab15d18e31526f8bf8871abdbc075a288
Shivam Saurav <shivamsaurav63@gmail.com>
2024-03-12 07:42:52 +0530

    Connected database

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ const { PORT } = require("./config/serverConfig")
 const setupAndStartServer = async () => {
     const app = express();
 
+    app.get("/",(req,res)=>{
+        res.status(200).send("hello");
+    })
+
     app.listen(PORT, ()=>{
         console.log(`Server started on ${PORT}`);
     })
