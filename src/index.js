8996e073edf9d826959576b84a99b3db17de0042
Shivam Saurav <shivamsaurav63@gmail.com>
2024-03-13 22:27:26 +0530

    Added the city model and created basic city repository

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 const express = require ("express");
-const { PORT } = require("./config/serverConfig")
+const bodyParser = require("body-parser");
+
+const { PORT } = require("./config/serverConfig");
 
 const setupAndStartServer = async () => {
     const app = express();
 
-    app.get("/",(req,res)=>{
-        res.status(200).send("hello");
-    })
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
 
-    app.listen(PORT, ()=>{
+
+    app.listen(PORT, async ()=>{
         console.log(`Server started on ${PORT}`);
     })
 }
